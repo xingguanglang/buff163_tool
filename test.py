c0c58668894f6c99b406826206d508e51d6feaba
github_name <github_email>
2024-03-14 19:44:04 +0800

    cs2 buff item lists

--- test.py
+++ test.py
@@ -1,29 +1,41 @@
-from matplotlib import pyplot as plt
-from find_url import generate_goods_url_list
-from item import buff_item,deep_partition
-from utils_storage import read_buff_item_from_json,write_item_to_json,read_item_from_json
-from item_filter import generate_item_list_from_deep_partition
-# a = read_buff_item_from_json()[10]
-# b = deep_partition(a,0.005)
+rarity_list = [("ancient_weapon","隐蔽"),("legendary_weapon","保密"),("mythical_weapon","受限"),("rare_weapon","军规级"),("uncommon_weapon","工业级"),("common_weapon","消费级")]
+rarity_list = [("ancient_weapon","隐蔽"),("legendary_weapon","保密"),("mythical_weapon","受限"),("rare_weapon","军规级")]
+quality_list = [("normal","普通")]
 
-# c = generate_item_list_from_deep_partition(b)
-# write_item_to_json(c)
-c = read_item_from_json()
-wears = [x.wear for x in c]
-prices = [x.price for x in c]
-slopes = [x.price/x.wear for x in c]
+exterior_list = [("wearcategory0","崭新出厂"),("wearcategory1","略有磨损"),("wearcategory2","久经沙场"),("wearcategory3","破损不堪"),("wearcategory4","战痕累累"),]
 
-fig, axs = plt.subplots(1, 2, figsize=(10, 5)) # 1行2列
+itemset_list = [("set_community_33","千瓦"),("set_community_32","变革"),
+                ("set_community_31","反冲"),("set_community_30","梦魇"),
+                ("set_community_29","激流大行动"),("set_community_28","蛇噬"),
+                ("set_community_27","狂牙大行动"),("set_community_26","裂空"),
+                ("set_community_25","棱彩2号"),("set_community_24","裂网大行动"),
+                ("set_community_23","反恐精英20周年"),("set_community_22","棱彩"),
+                ("set_community_21","头号特训"),("set_community_20","地平线"),
+                ("set_community_19","命悬一线"),("set_community_18","光谱2号"),
+                ("set_community_17","九头蛇大行动"),("set_community_16","光谱"),
+                ("set_community_15","手套武器箱"),("set_community_14","伽马2号"),
+                ("set_community_13","伽马"),("set_community_12","幻彩3号"),
+                ("set_community_11","野火大行动"),("set_community_10","左轮武器箱"),
+                ("set_community_9","暗影"),("set_community_8","弯曲猎手"),
+                ("set_community_7","幻彩2号"),("set_community_6","幻彩"),
+                ("set_community_5","先锋"),("set_community_4","突围"),
+                ("set_community_3","猎杀者"),("set_community_2","凤凰"),
+                ("set_community_1","冬季攻势"),
+                ("set_esports","电竞2013"),("set_esports_ii","电竞2013冬季"),("set_esports_iii","电竞2014夏季"),
+                ("set_weapons_i","反恐精英武器箱"),("set_weapons_ii","反恐精英2号武器箱"),("set_weapons_iii","反恐精英3号武器箱"),
+                ("set_bravo_i","英勇武器箱")
+                ]
 
-# 第一个散点图
-axs[0].scatter(wears, prices)
-axs[0].set_title('第一幅散点图')
-axs[0].set_xlabel('list a1')
-axs[0].set_ylabel('list b1')
-
-# 第二个散点图
-axs[1].scatter(wears, slopes)
-axs[1].set_title('第二幅散点图')
-axs[1].set_xlabel('list a2')
-axs[1].set_ylabel('list b2')
-plt.show()
+itemset_list [("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+              ("set_",""),("set_",""),("set_",""),
+]
