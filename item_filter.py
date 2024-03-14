49e753eaa8e1756fde55c5293cdc72112f4e5c5e
github_name <github_email>
2024-03-14 19:43:09 +0800

    fix bugs

--- item_filter.py
+++ item_filter.py
@@ -1,13 +1,12 @@
 from playwright.sync_api import sync_playwright,TimeoutError
-from utils_storage import read_item_from_json, write_item_to_json,read_buff_item_from_json
-from utils_operation import remove_redundancy
+from utils_storage import  write_item_to_json
 from item import item,buff_item,deep_partition
 from find_url import generate_goods_url,generate_goods_url_list
 from datetime import datetime
 from time import time,sleep
 
-filepath = "json/storage.json"
-sleeptime=1
+sleeptime = 1
+
 def generate_item_list_from_buff_item(buff_item:buff_item, selector:str = '#market-selling-list > tbody>.selling',usr_data_dir = "browser_buffer/chrome"):
     url = generate_goods_url(buff_item)
     print("url:",url)
@@ -15,29 +14,41 @@ def generate_item_list_from_buff_item(buff_item:buff_item, selector:str = '#mark
         start = time()
         browser = p.chromium.launch_persistent_context(usr_data_dir,headless=True)
         page = browser.new_page()
-        page.goto(url)
         result = []
+        try: 
+            page.goto(url)
+        except(TimeoutError):
+            return result
+        
         try:
             page.wait_for_selector(selector,timeout=2500)
         except(TimeoutError):
-            sleep(sleeptime)
-            page.reload()
-            try:
-                page.wait_for_selector(selector,timeout=2500)
-            except(TimeoutError):
-                print(f"找不到元素: 名字:{buff_item.name}, buff_id:{buff_item.buff_id}, 等级:{buff_item.rarity}, 外观:{buff_item.exterior}, 收藏品:{buff_item.itemset}, 花费时间:{time()-start}s")
-                return 1
+            times = 3
+            while (True):
+                sleep(sleeptime)
+                page.reload()
+                try:
+                    page.wait_for_selector(selector,timeout=2500)
+                except(TimeoutError):
+                    print(f"找不到元素: 名字:{buff_item.name}, buff_id:{buff_item.buff_id}, 等级:{buff_item.rarity}, 外观:{buff_item.exterior}, 收藏品:{buff_item.itemset}, 花费时间:{time()-start}s")
+                    if times == 0 :
+                        return 1
+                    times = times - 1
+                else:
+                    break
         value = page.locator(selector).all()
         
         print(f"找到元素: 名字:{buff_item.name}, buff_id:{buff_item.buff_id}, 等级:{buff_item.rarity}, 外观:{buff_item.exterior}, 收藏品:{buff_item.itemset}")
-        for i in value:
-            a = i.locator("td:nth-child(3) > div > div.csgo_value > div.wear-value").inner_text().split(" ")[1]
-            b = i.locator(" td:nth-child(5) > div:nth-child(1) > strong").inner_text().split(" ")[1]
-            c = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
-            print(f"价格:{b}, 磨损:{a}")
-            tmp = item(buff_item.name,float(a),float(b),c)
-            result.append(tmp)
-        print(f"花费时间:{time()-start}s")
+        for y in range(len(value)):
+            i = value[y]
+            if y <3:
+                a = i.locator("td:nth-child(3) > div > div.csgo_value > div.wear-value").inner_text().split(" ")[1]
+                b = i.locator(" td:nth-child(5) > div:nth-child(1) > strong").inner_text().split(" ")[1]
+                c = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+                print(f"价格:{b}, 磨损:{a}")
+                tmp = item(buff_item.name,float(a),float(b),c)
+                result.append(tmp)
+                print(f"花费时间:{time()-start}s")
     return result
 
 def generate_item_list_from_deep_partition(deep_partition:deep_partition, selector:str = '#market-selling-list > tbody>.selling',usr_data_dir = "browser_buffer/chrome"):
@@ -50,37 +61,43 @@ def generate_item_list_from_deep_partition(deep_partition:deep_partition, select
             start = time()
             browser = p.chromium.launch_persistent_context(usr_data_dir,headless=True)
             page = browser.new_page()
-            page.goto(x)
+            try :
+                page.goto(x)
+            except(TimeoutError):
+                continue
             try:
                 page.wait_for_selector(selector,timeout=2500)
             except(TimeoutError):
-                sleep(sleeptime)
-                page.reload()
-                try:
-                    page.wait_for_selector(selector,timeout=2500)
-                except(TimeoutError):
-                    print(f"找不到元素: 名字:{buff_item.name}, buff_id:{buff_item.buff_id}, 等级:{buff_item.rarity}, 外观:{buff_item.exterior}, 收藏品:{buff_item.itemset}, 花费时间:{time()-start}s")
-                    return 1
+                times = 3
+                while (True):
+                    sleep(sleeptime)
+                    page.reload()
+                    try:
+                        page.wait_for_selector(selector,timeout=2500)
+                    except(TimeoutError):
+                        print(f"找不到元素: 名字:{buff_item.name}, buff_id:{buff_item.buff_id}, 等级:{buff_item.rarity}, 外观:{buff_item.exterior}, 收藏品:{buff_item.itemset}, 花费时间:{time()-start}s")
+                        if times == 0:
+                            return 1
+                        times = times - 1
+                    else:
+                        break
             value = page.locator(selector).all()
             
             print(f"找到元素: 名字:{buff_item.name}, buff_id:{buff_item.buff_id}, 等级:{buff_item.rarity}, 外观:{buff_item.exterior}, 收藏品:{buff_item.itemset}")
-            for i in value:
-                a = i.locator("td:nth-child(3) > div > div.csgo_value > div.wear-value").inner_text().split(" ")[1]
-                b = i.locator(" td:nth-child(5) > div:nth-child(1) > strong").inner_text().split(" ")[1]
-                c = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
-                print(f"价格:{b}, 磨损:{a}")
-                tmp = item(buff_item.name,float(a),float(b),c)
-                result.append(tmp)
-            print(f"花费时间:{time()-start}s")
+            for y in range(len(value)):
+                i = value[y]
+                if y <3:
+                    a = i.locator("td:nth-child(3) > div > div.csgo_value > div.wear-value").inner_text().split(" ")[1]
+                    b = i.locator(" td:nth-child(5) > div:nth-child(1) > strong").inner_text().split(" ")[1]
+                    c = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+                    print(f"价格:{b}, 磨损:{a}")
+                    tmp = item(buff_item.name,float(a),float(b),c)
+                    result.append(tmp)
+                    print(f"花费时间:{time()-start}s")
     return result
 
-
-
-
-if __name__ == '__main__':
-    # generate_item_list_from_buff_item_list(buff_item(name="M4A1 消音型 | 黑莲花 (略有磨损)", exterior="略有磨损", buff_id="956471", itemset="千瓦", rarity="军规级"))
+def create_item_lists_from_buff_item_list(buff_item_list:list[buff_item]):
     value = []
-    buff_item_list = read_buff_item_from_json()
     count = 0 
     for i in buff_item_list:
         tmp = generate_item_list_from_buff_item(i)
@@ -88,11 +105,25 @@ if __name__ == '__main__':
             for j in tmp:
                 value.append(j)
         count = count + 1
-        print(f"已经完成:{count}, 总数量:{len(buff_item_list)}, 完成百分比:{'{:.2f}%'.format(count*100/len(buff_item_list))}")
+        print(f"已经完成:{count} items , 总数量:{len(buff_item_list)}, 完成百分比:{'{:.2f}%'.format(count*100/len(buff_item_list))}")
         sleep(sleeptime)
-    # loads = read_item_from_json(filepath)
-    # result = remove_redundancy(value,loads)
-    filepath = filepath+datetime.now().strftime("%Y-%m-%d,%H:%M:%S")+".json"
-    write_item_to_json(value,filepath)
+    write_item_to_json(value)
     print("finish")
-    #generate item with price
+    return value
+
+def create_item_lists_from_deep_partition_list(deep_partition_list:list[deep_partition]):
+    value = []
+    count = 0 
+    for i in deep_partition_list:
+        tmp = generate_item_list_from_deep_partition(i)
+        if tmp !=1:
+            for j in tmp:
+                value.append(j)
+        count = count + 1
+        print(f"item_list 已经完成:{count}, 总数量:{len(deep_partition_list)}, 完成百分比:{'{:.2f}%'.format(count*100/len(deep_partition_list))}")
+        sleep(sleeptime)
+
+    write_item_to_json(value)
+    print("finish")
+    return value
+
